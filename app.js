b16a526153c5ca05fcd5bb57cd22644ec3078683
sharathnair9999 <sharathnair9999@gmail.com>
2021-09-15 16:57:16 +0530

    error msg

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ var myBtn = document.querySelector("#myButton")
 var txtInput = document.querySelector("#txt-input")
 var txtOutput = document.querySelector("#txt-output")
 
+function errorHandler(error){
+  alert("Uh..Oh!! Something didn't work as it should", error)
+}
+
 function fetchResult(){
   var url = "https://api.funtranslations.com/translate/minion.json?text="+txtInput.value
   console.log(url)
-  fetch(url).then(response=>response.json()).then(json=>txtOutput.innerText=json.contents.translated)
+  fetch(url).then(response=>response.json()).then(json=>txtOutput.innerText=json.contents.translated).catch((errorHandler))
   // console.log("Sharath", txtInput.value)
 }
 myBtn.addEventListener("click", fetchResult)
